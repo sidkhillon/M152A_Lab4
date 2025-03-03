87ad440173b7257b450d129e8f38c7d3f11d771d
Siddharth Khillon <sidkhillon24@gmail.com>
2025-03-03 12:58:38 -0800

    Latched start

--- Countdown.v
+++ Countdown.v
@@ -1,4 +1,5 @@
 module Countdown(
+    input wire clk,
     input wire clk_countdown,
     input wire rst,
     input wire start,
@@ -7,15 +8,26 @@ module Countdown(
     output reg countdown_in_action
 );
 
+reg start_latched;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        start_latched <= 0;
+    end else begin
+        start_latched <= 1;
+    end
+end
+
 always @(posedge clk_countdown or posedge rst) begin
     if (rst) begin
         anode <= 2'b00;
         countdown_done <= 0;
         countdown_in_action <= 0;
-    end else if (start) begin
+    end else if (start_latched) begin
         anode <= 2'b00;
         countdown_done <= 0;
         countdown_in_action <= 1;
+        start_latched <= 0;
     end else begin
         if (countdown_in_action) begin
             if (anode == 2'd3) begin
